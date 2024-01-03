6d5294f51aca6cf3531710b99dce40b4fecfb1e8
Harsh Rajput <harshmori0521@gmail.com>
2024-01-03 13:44:55 +0530

    responsive nav bar for desktop

--- lib/desktop.dart
+++ lib/desktop.dart
@@ -33,82 +33,96 @@ class _DesktopState extends State<Desktop> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          "assets/images/logos/se_logo.png",
-                          fit: BoxFit.cover,
-                          width:
-                              mq.width * 0.1 > 200.0 ? mq.width * 0.2 : 200.0,
-                          height:
-                              mq.height * 0.1 > 200.0 ? mq.height * 0.2 : 200.0,
-                        ),
-                        SizedBox(
-                          width: mq.width * 0.05,
-                        ),
-                        Navbaritem(onPressed: () {}, optionName: "Home"),
-                        Navbaritem(onPressed: () {}, optionName: "About"),
-                        Navbaritem(onPressed: () {}, optionName: "Products"),
-                        Navbaritem(onPressed: () {}, optionName: "Facility"),
-                        Navbaritem(onPressed: () {}, optionName: "Contact"),
-                      ],
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/images/logos/se_logo.png",
+                            fit: BoxFit.cover,
+                            width:
+                                mq.width * 0.1 > 200.0 ? mq.width * 0.2 : 200.0,
+                            height:
+                                mq.height * 0.1 > 200.0 ? mq.height * 0.2 : 200.0,
+                          ),
+                          SizedBox(
+                            width: mq.width * 0.01,
+                          ),
+                          Navbaritem(onPressed: () {}, optionName: "Home"),
+                          Navbaritem(onPressed: () {}, optionName: "About"),
+                          Navbaritem(onPressed: () {}, optionName: "Products"),
+                          Navbaritem(onPressed: () {}, optionName: "Facility"),
+                          Navbaritem(onPressed: () {}, optionName: "Contact"),
+                        ],
+                      ),
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                    child: Row(
-                      children: [
-                        Row(
-                          children: [
-                            MouseRegion(
-                              onExit: (_) {
-                                setState(() {
-                                  isHover1 = false;
-                                });
-                              },
-                              onEnter: (_) {
-                                setState(() {
-                                  isHover1 = true;
-                                });
-                              },
-                              child: IconButton(
-                                onPressed: (){
-
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Row(
+                            children: [
+                              MouseRegion(
+                                onExit: (_) {
+                                  setState(() {
+                                    isHover1 = false;
+                                  });
                                 },
-                                icon: Icon(Icons.file_download_outlined,size: 32,),color:isHover1 ? AppColors.theme['highlightColor'] : AppColors.theme['secondaryColor'] ,
+                                onEnter: (_) {
+                                  setState(() {
+                                    isHover1 = true;
+                                  });
+                                },
+                                child: IconButton(
+                                  onPressed: (){
+
+                                  },
+                                  icon: Icon(Icons.file_download_outlined,size: 32,),color:isHover1 ? AppColors.theme['highlightColor'] : AppColors.theme['secondaryColor'] ,
+                                ),
+                              ),
+                              Text("Download Brochure",style: TextStyle(color: AppColors.theme['secondaryColor']),),
+                            ],
+                          ),
+                          SizedBox(width: mq.width*0.02,),
+                          MouseRegion(
+                            onExit: (_) {
+                              setState(() {
+                                isHover2 = false;
+                              });
+                            },
+                            onEnter: (_) {
+                              setState(() {
+                                isHover2 = true;
+                              });
+                            },
+                            child: Container(
+                              constraints:BoxConstraints(
+                                maxHeight: 50,
+                                maxWidth: 150,
+                                minWidth: 110
+                              ),
+                              width: mq.width*0.09,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  minimumSize: MaterialStateProperty.all(Size(50, 50)),
+                                  backgroundColor: isHover2 ? MaterialStateProperty.all(AppColors.theme['highlightColor']):  MaterialStateProperty.all(AppColors.theme['secondaryColor']),
+                                ),
+                                child: Text(
+                                  "inquiry",
+                                  style: TextStyle(fontSize: 16, color: isHover2 ?AppColors.theme['secondaryColor'] :Colors.black),
+                                ),
                               ),
                             ),
-                            Text("Download Brochure",style: TextStyle(color: AppColors.theme['secondaryColor']),),
-                          ],
-                        ),
-                        SizedBox(width: mq.width*0.02,),
-                        MouseRegion(
-                          onExit: (_) {
-                            setState(() {
-                              isHover2 = false;
-                            });
-                          },
-                          onEnter: (_) {
-                            setState(() {
-                              isHover2 = true;
-                            });
-                          },
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                              minimumSize: MaterialStateProperty.all(Size(150, 50)),
-                              backgroundColor: isHover2 ? MaterialStateProperty.all(AppColors.theme['highlightColor']):  MaterialStateProperty.all(AppColors.theme['secondaryColor']),
-                            ),
-                            child: Text(
-                              "Inquire Now",
-                              style: TextStyle(fontSize: 16, color: isHover2 ?AppColors.theme['secondaryColor'] :Colors.black),
-                            ),
                           ),
-                        ),
 
 
-                      ],
+                        ],
+                      ),
                     ),
                   )
                 ],
